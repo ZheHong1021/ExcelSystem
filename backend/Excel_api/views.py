0fd7ef7113cd2aa26d3ffdfde9163ccee822c44c
unknown <m10y036@mirdc.org>
2021-11-29 16:54:47 +0800

    excel

--- backend/Excel_api/views.py
+++ backend/Excel_api/views.py
@@ -13,21 +13,22 @@ class ControlExcel(View):
     def post(self, request):
         pythoncom.CoInitialize()
         book = request.POST
+        print(book)
         dict = {'E':5,'G':7,'I':9,'K':11,'M':13,'O':15,'Q':17}
 
-        read_excel = xlrd.open_workbook('../frontend/public/excel_folder/S01.xlsx')
-        table = read_excel.sheet_by_name('1-1')
+        read_excel = xlrd.open_workbook('../frontend/public/excel_folder/'+book['select_loop']+'.xlsx')
+        table = read_excel.sheet_by_name(book['select_plant'])
         write_excel = xw.App(visible=True,add_book=False)
-        wb = write_excel.books.open('../frontend/public/excel_folder/S01.xlsx')
-
-        for i in dict:
-            if xlrd.xldate.xldate_as_datetime(table.cell_value(4, dict[i]),0) == datetime.datetime.strptime(book['excel_date'],"%Y-%m-%d"):
-                print(xlrd.xldate.xldate_as_datetime(table.cell_value(4, dict[i]),0))
-                wb.sheets[book['sheet']].range(i.split(',')[0]+'7').value = book['question_one_value']
-                wb.save()
-                break
-        wb.close()
-        write_excel.quit()
+        wb = write_excel.books.open('../frontend/public/excel_folder/'+book['select_loop']+'.xlsx')
+        print(xlrd.xldate.xldate_as_datetime(table.cell_value(3, 4),0),datetime.datetime.strptime(book['select_date'],"%Y-%m-%d"))
+        # for i in dict:
+        if xlrd.xldate.xldate_as_datetime(table.cell_value(3, 4),0) == datetime.datetime.strptime(book['select_date'],"%Y-%m-%d"):
+            print('hi')
+            # wb.sheets[book['sheet']].range(i.split(',')[0]+'7').value = book['question_one_value']
+            # wb.save()
+            # break
+        # wb.close()
+        # write_excel.quit()
         return JsonResponse(book, safe=False)
 
 class GetExcel(View):
@@ -43,16 +44,8 @@ class GetExcel(View):
                 self.all_sheet.append(sheet.name)
             self.sheet_info = self.read_excel.sheet_by_name(self.all_sheet[0])
             for i in [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,25,26]:
-                print(self.sheet_info.cell_value(i, 3))
                 self.excel_info[self.sheet_info.cell_value(i, 2)] = [self.sheet_info.cell_value(i, 1),self.sheet_info.cell_value(i, 3)]
 
             self.all_xlsx[os.path.splitext(xlsx)[0]] = [self.all_sheet,self.excel_info,0]
 
         return JsonResponse(self.all_xlsx, safe=False)
-
-    def post(self, request):
-        pythoncom.CoInitialize()
-        excel_info = request.POST
-        read_excel = xlrd.open_workbook('../frontend/public/excel_folder/S01.xlsx')
-        table = read_excel.sheet_by_name(excel_info['select_sheet'])
-        return JsonResponse(excel_info, safe=False)
--- frontend/src/components/excel_control.vue
+++ frontend/src/components/excel_control.vue
@@ -8,7 +8,7 @@
                 </div>
                 <div class="col-4">
                     <label for="select_label" class="form-label py-3">迴路選擇</label>
-                    <select class="form-select" v-model="select_file" id="select_label" v-on:change="get_excel">
+                    <select class="form-select" v-model="select_file" id="select_label" v-on:change="function() {sheet_info = JSON.parse(JSON.stringify(xlsx_name[select_file][0]));}">
                         <option v-for="(xlsx, index) in xlsx_name" :key="index">{{ index }}</option>
                     </select>
                 </div>
@@ -19,27 +19,39 @@
                     </select>
                 </div>
             </div>
-            <div class="row justify-content-center" v-for="(project, index) in project_info" :key="index" :vlaue="project">
-                <div class="col-2 mt-5">
-                    <h4>{{ index }}</h4>
+            <div style="overflow-y:scroll; height:70vh; overflow-x: hidden;" class="mt-5">
+                <div class="row justify-content-center" v-for="(project, index) in project_info" :key="index" :vlaue="project">
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-xs-12 mt-5">
+                        <h4>{{ index }}</h4>
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-xs-12">
+                        <label for="question_one" class="form-label py-3">權重比(%)</label>
+                        <input class="form-control" id="question_one_value" type="text" :value="String(project[0]*100)+'%'" readonly>
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-xs-12">
+                        <label for="question_one" class="form-label py-3">總量</label>
+                        <input class="form-control" id="question_one_value" type="text" :value="project[1]" readonly>
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-xs-12">
+                        <label for="question_one" class="form-label py-3">完工量</label>
+                        <input class="form-control" id="question_one_value" type="text" placeholder="完成量" v-model="project[2]">
+                    </div>  
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-xs-12">
+                        <label for="question_one" class="form-label py-3">完工率(%)</label>
+                        <input class="form-control" id="question_one_value" type="text" :value="String((project[2]/project[1])*100)+'%'" readonly>
+                    </div>    
                 </div>
-                <div class="col-2">
-                    <label for="question_one" class="form-label py-3">權重比(%)</label>
-                    <input class="form-control" id="question_one_value" type="text" placeholder="完成量" aria-label="default input example" :value="String(project[0]*100)+'%'" readonly>
-                </div>
-                <div class="col-2">
-                    <label for="question_one" class="form-label py-3">總量</label>
-                    <input class="form-control" id="question_one_value" type="text" placeholder="完成量" aria-label="default input example" :value="project[1]" readonly>
-                </div>
-                <div class="col-2">
-                    <label for="question_one" class="form-label py-3">完工量</label>
-                    <input class="form-control" id="question_one_value" type="text" placeholder="完成量" v-model="project[2]" aria-label="default input example">
-                </div>  
-                <div class="col-2">
-                    <label for="question_one" class="form-label py-3">完工率(%)</label>
-                    <input class="form-control" id="question_one_value" type="text" placeholder="完成量" :value="String((project[2]/project[1])*100)+'%'" aria-label="default input example" readonly>
-                </div>     
             </div>
+            <center class="mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-5">
+                        <button type="button" class="btn btn-danger" style="padding:2vh 5vw">清除</button>
+                    </div>
+                    <div class="col-5">
+                        <button type="button" class="btn btn-primary" style="padding:2vh 5vw" v-on:click="post_excel">確認</button>
+                    </div>
+                </div>
+            </center>
         </div>
     </header>
 </template>
@@ -52,11 +64,10 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 export default {
     data(){
         return{
-            data_json: new FormData(),
             excel_date:'',
             select_file:'',
             select_sheet:'',
-            xlsx_name:"",
+            xlsx_name:'',
             sheet_info:[],
             project_info:[],
         }
@@ -65,7 +76,7 @@ export default {
         this.excel_info()
     },
     methods:{
-        excel_info:async function() {
+        excel_info:async function() { //取得excel相關資訊(權重、項目名稱等..)
             axios.get('/api/excel_read/').then(
                 response => {
                     this.xlsx_name = response.data
@@ -75,48 +86,39 @@ export default {
                 }
             )
         },
-        get_excel:async function() {
-            let formData = new FormData()
-            formData.append('select_file',this.select_file)
-            this.sheet_info = JSON.parse(JSON.stringify(this.xlsx_name[this.select_file][0]));
-            // axios({
-            //     method: 'post',
-            //     url: '/api/excel_read/',
-            //     xstfCookieName: 'csrftoken',
-            //     xsrfHeaderName: 'X-CSRFToken',
-            //     data: formData,
-            //     headers: {
-            //         'X-CSRFToken': 'csrftoken',
-            //     }
-            // }).then(response => console.log(response));
-        },
-        post_excel:async function() {
-            let formData = new FormData()
-            formData.append('question_one_value',this.question_one_value)
-            formData.append('question_one_percent',this.question_one_percent)
-            formData.append('question_two_value',this.question_two_value)
-            formData.append('question_two_percent',this.question_two_percent)
-            formData.append('sheet',this.sheet)
-            formData.append('excel_date',this.excel_date)
 
-            axios({
+        post_excel:async function() {  //將使用者填表單資料送到後端
+            let formData = new FormData;
+            formData.append('select_date',this.excel_date)
+            formData.append('select_loop',this.select_file)
+            formData.append('select_plant',this.select_sheet)
+            for (var i in this.project_info) {
+                formData.append(i,[this.project_info[i][2],Number(this.project_info[i][2])/Number(this.project_info[i][1])*100])
+            }
+            console.log(formData)
+            axios({ 
                 method: 'post',
                 url: '/api/excel_write/ ',
                 xstfCookieName: 'csrftoken',
                 xsrfHeaderName: 'X-CSRFToken',
                 data: formData,
+                responseType: 'json',
                 headers: {
                     'X-CSRFToken': 'csrftoken',
                 }
             }).then(response => console.log(response));
             window.alert('輸入成功')
         },
-    }
+
+    },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    header {
+        height: auto;
+    }
     h4 {
         color: white;
     }
